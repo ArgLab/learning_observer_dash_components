84ffd344808719b138311564774f4bd4b8226690
Brad Erickson <bbwe24@gmail.com>
2022-10-21 14:00:51 -0400

    Reran build on my system

---
+++ R/'loc'LOConnection.R
@@ -0,0 +1,19 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+#' @export
+'loc'LOConnection <- function(id=NULL, error=NULL, key=NULL, message=NULL, send=NULL, state=NULL, url=NULL) {
+    
+    props <- list(id=id, error=error, key=key, message=message, send=send, state=state, url=url)
+    if (length(props) > 0) {
+        props <- props[!vapply(props, is.null, logical(1))]
+    }
+    component <- list(
+        props = props,
+        type = 'LOConnection',
+        namespace = 'learning_observer_components',
+        propNames = c('id', 'error', 'key', 'message', 'send', 'state', 'url'),
+        package = 'learningObserverComponents'
+        )
+
+    structure(component, class = c('dash_component', 'list'))
+}
---
+++ src/jl/'loc'_loconnection.jl
@@ -0,0 +1,24 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+export 'loc'_loconnection
+
+"""
+    'loc'_loconnection(;kwargs...)
+
+A LOConnection component.
+A simple interface to
+Keyword arguments:
+- `id` (String; optional): The ID used to identify this component in Dash callbacks.
+- `error` (Dict | String; optional): This property is set with the content of the onerror event.
+- `key` (Dict; optional): Supported websocket key (optional).
+- `message` (Dict | String; optional): When messages are received, this property is updated with the message content.
+- `send` (Dict | String; optional): When this property is set, a message is sent with its content.
+- `state` (Dict | String; optional): This websocket state (in the readyState prop) and associated information.
+- `url` (String; optional): The websocket endpoint (e.g. wss://echo.websocket.org).
+"""
+function 'loc'_loconnection(; kwargs...)
+        available_props = Symbol[:id, :error, :key, :message, :send, :state, :url]
+        wild_props = Symbol[]
+        return Component("'loc'_loconnection", "LOConnection", "learning_observer_components", available_props, wild_props; kwargs...)
+end
+
