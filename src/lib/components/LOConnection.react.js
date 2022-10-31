191172a26cabf0457054b788ec3e159d5207fc1b
Brad Erickson <bbwe24@gmail.com>
2022-10-31 09:44:07 -0400

    More debugging

--- src/lib/components/LOConnection.react.js
+++ src/lib/components/LOConnection.react.js
@@ -82,13 +82,14 @@ export default class LOConnection extends Component {
 
     componentDidUpdate(prevProps) {
         const {send, data_scope} = this.props;
+        console.log('prevProps', prevProps);
         // Send messages.
         if (send && send !== prevProps.send) {
             if (this.props.state.readyState === WebSocket.OPEN) {
                 this.client.send(send)
             }
         }
-        console.log(data_scope, prevProps.data_scope);
+        console.log('datascope', data_scope);
         // TODO: Maybe add support for changing the url?
         if (!_.isEqual(data_scope, prevProps.data_scope)) {
             console.log('closing');
